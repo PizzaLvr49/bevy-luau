a2f5581bdc48e1000fc5e0d879bd124d28b8bc88
PizzaLvr49 <jhtodd2012@gmail.com>
2026-06-27 14:46:41 -0400

    added command to spawn a build luau query

--- src/query.rs
+++ src/query.rs
@@ -1,3 +1,5 @@
+use std::convert::Infallible;
+
 use bevy::{
     ecs::{component::ComponentId, query::FilteredAccess, world::FilteredEntityMut},
     prelude::*,
@@ -9,7 +11,7 @@ use smallvec::SmallVec;
 #[derive(Component)]
 pub struct LuaQueryEntry {
     /// Descriptor of the query
-    pub descriptor: FilteredAccess,
+    pub access: FilteredAccess,
     /// The cached state
     pub state: QueryState<FilteredEntityMut<'static, 'static>>,
     /// The order of components
@@ -19,6 +21,34 @@ pub struct LuaQueryEntry {
 /// The lua components a entity has
 #[derive(Component, Default)]
 pub struct LuaComponents {
-    /// The list of keys to the cached lua state for this entities components
+    /// The list of keys to the cached lua state for this entity's components
     pub keys: SmallVec<[(ComponentId, LuaRegistryKey); 8]>,
+}
+
+/// A command that constructs and spawns a [`LuaQueryEntry`]
+pub struct BuildLuauQuery {
+    /// The query descriptor describing what components and filters the query has
+    pub access: FilteredAccess,
+    /// The order of the components in the query
+    pub order: SmallVec<[ComponentId; 8]>,
+}
+
+impl Command for BuildLuauQuery {
+    type Out = Result<Entity, Infallible>;
+
+    fn apply(self, world: &mut World) -> Self::Out {
+        let mut builder = QueryBuilder::<FilteredEntityMut>::new(world);
+        builder.extend_access(self.access.clone());
+        let state: QueryState<FilteredEntityMut<'static, 'static>> = builder.build();
+
+        let entity = world
+            .spawn(LuaQueryEntry {
+                access: self.access,
+                state,
+                order: self.order,
+            })
+            .id();
+
+        Ok(entity)
+    }
 }
