192589eb99bc4d693d0ee774fa57f16967fbb322
PizzaLvr49 <jhtodd2012@gmail.com>
2026-06-27 11:04:42 -0400

    refactor to use more of the ecs

--- src/query.rs
+++ src/query.rs
@@ -1,21 +1,24 @@
 use bevy::{
-    ecs::{entity::EntityHashMap, query::FilteredAccess},
+    ecs::{component::ComponentId, query::FilteredAccess, world::FilteredEntityMut},
     prelude::*,
 };
 use mluau::prelude::*;
 use smallvec::SmallVec;
 
 /// A cached luau query state
-pub struct LuaQueryCache {
-    /// Maps an entity to its components
-    pub rows: EntityHashMap<SmallVec<[LuaTable; 16]>>,
-}
-
-/// A luau query descriptor and its associated cache
 #[derive(Component)]
 pub struct LuaQueryEntry {
-    /// The descriptor
+    /// Descriptor of the query
     pub descriptor: FilteredAccess,
-    /// The cache
-    pub cache: LuaQueryCache,
+    /// The cached state
+    pub state: QueryState<FilteredEntityMut<'static, 'static>>,
+    /// The order of components
+    pub order: SmallVec<[ComponentId; 8]>,
+}
+
+/// The lua components a entity has
+#[derive(Component, Default)]
+pub struct LuaComponents {
+    /// The list of keys to the cached lua state for this entities components
+    pub keys: SmallVec<[(ComponentId, LuaRegistryKey); 8]>,
 }
